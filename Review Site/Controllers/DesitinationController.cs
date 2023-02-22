229ef2d11e265266711f5d88435633cc10f85e63
sergeiklein <sjklein088@gmail.com>
2023-02-22 13:39:53 -0800

    Added CRUD methods to controllers

--- Review Site/Controllers/DesitinationController.cs
+++ Review Site/Controllers/DesitinationController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Review_Site.Data;
 using Review_Site.Models;
 
@@ -31,6 +32,61 @@ namespace Review_Site.Controllers
                 _context.SaveChanges();
                 return RedirectToAction("Index");
             }
+            return View(destination);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var destination = _context.Destinations.FirstOrDefault(
+                g => g.Id == id);
+            if (destination == null)
+            {
+                return NotFound();
+            }
+            return View(destination);
+        }
+
+        //the http delete method doesnt delete the specified game, it returns a view of the game wher you can submit the deletion
+        [HttpPost]
+        public ActionResult Delete(int? id, bool notUsed)
+        {
+            if (_context.Destinations == null)
+            {
+                return Problem("Entity set 'GameContext.BoardGames is null");
+            }
+            var destination = _context.Destinations.Find(id);
+            _context.Destinations.Remove(destination);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, DestinationModel destination)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(destination).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(destination);
+        }
+
+        public ActionResult Details(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var destination = _context.Destinations.Find(id);
             return View(destination);
         }
     }
--- Review Site/Controllers/ReviewController.cs
+++ Review Site/Controllers/ReviewController.cs
@@ -1,12 +1,94 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Review_Site.Data;
+using Review_Site.Models;
 
 namespace Review_Site.Controllers
 {
     public class ReviewController : Controller
     {
-        public IActionResult Index()
+        private readonly ReviewSiteContext _context;
+
+        public ReviewController(ReviewSiteContext context)
+        {
+            _context = context;
+        }
+        public ActionResult Index()
+        {
+            return View(_context.Reviews.ToList());
+        }
+
+        public ActionResult Create()
         {
             return View();
         }
+        [HttpPost]
+
+        public ActionResult Create(ReviewModel reviews)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Reviews.Add(reviews);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(reviews);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var reviews = _context.Reviews.FirstOrDefault(
+                g => g.Id == id);
+            if (reviews == null)
+            {
+                return NotFound();
+            }
+            return View(reviews);
+        }
+
+        //the http delete method doesnt delete the specified game, it returns a view of the game wher you can submit the deletion
+        [HttpPost]
+        public ActionResult Delete(int? id, bool notUsed)
+        {
+            if (_context.Reviews == null)
+            {
+                return Problem("Entity set 'GameContext.BoardGames is null");
+            }
+            var reviews = _context.Reviews.Find(id);
+            _context.Reviews.Remove(reviews);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, ReviewModel review)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(review).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(review);
+        }
+
+        public ActionResult Details(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var review = _context.Reviews.Find(id);
+            return View(review);
+        }
     }
+
 }

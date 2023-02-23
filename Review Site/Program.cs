001a4c3e129a26e95bf58e5a59d543208158773c
Mathew Cowdery <mcowdery@gmail.com>
2023-02-23 15:51:57 -0500

    Changed Home Page

--- Review Site/Program.cs
+++ Review Site/Program.cs
@@ -23,8 +23,9 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Destination}/{action=Index}/{id?}");
 
 app.Run();

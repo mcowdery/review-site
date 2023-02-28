97e8ed0de9f740a19b79ab79d89d23e8d8d8e872
Mathew Cowdery <mcowdery@gmail.com>
2023-02-28 15:55:52 -0500

    CSS styling for Destination page

--- Review Site/Data/ReviewSiteContext.cs
+++ Review Site/Data/ReviewSiteContext.cs
@@ -24,7 +24,7 @@ namespace Review_Site.Data
                     Name = "Tour of Tokyo",
                     City = "Tokyo",
                     Country = "Japan",
-                    Description = "See Imperial Palace, Hachikō Memorial Statue, Unicorn Gundam Statue, Senso-Ji Temple and so much more",
+                    Description = "Discover Tokyo at your own pace on this 4- or 8-hour private, custom tour with guide. Design a tour that’s perfect for your interests. There’s a world of options, including the outer fish market at Tsukiji, the city’s temples and shrines, shopping excursions and food or sake tours. Your private custom tour includes a guide, entrance fees at select attractions, snacks, round-trip transport from your hotel with meet-and-greet and a car (for the private transfer option), or public transport (for the public transfer) and optional lunch (for the full-day tour).",
                     ImageURL = "\\Images\\Tokyo1.jpg",
                     Price = 8000,
                     TourDuration = 10

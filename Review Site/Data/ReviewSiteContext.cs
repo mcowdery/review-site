88855d6eef79ce10674c2c3848b9535d08fda9c1
sergeiklein <sjklein088@gmail.com>
2023-03-03 06:58:19 -0800

    fill in seed data

--- Review Site/Data/ReviewSiteContext.cs
+++ Review Site/Data/ReviewSiteContext.cs
@@ -12,7 +12,7 @@ namespace Review_Site.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string connectionString = "Server=(localdb)\\mssqllocaldb;Database=ReviewSite30;Trusted_Connection=True;";
+            string connectionString = "Server=(localdb)\\mssqllocaldb;Database=ReviewSite32;Trusted_Connection=True;";
             optionsBuilder.UseSqlServer(connectionString);
         }
 
@@ -63,32 +63,181 @@ namespace Review_Site.Data
                     Country = "Unknown",
                     Description = "See what no man in history has seen. Be the first to discover the Red Planet. This is a once in a lifetime opportunity.  Travel along with the one and only Elon Musk himself on multibillion dollar spaceship x. Its dangerous, its thrilling, its ridiculous. You can be the first to set your foot and leave human mark on the red planet. Do you have what it takes? Mainly the money? Go and Find out! ",
                     ImageURL = "\\Images\\Mars1.jpg",
-                    ImageList = "\\Images\\Rome1.jpg,\\Images\\Tokyo1.jpg,\\Images\\Mars.jpg",
+                    ImageList = "\\Images\\Mars11.jpg,\\Images\\Mars12.jpg,\\Images\\Mars13.jpg,\\Images\\Mars14.jpg,\\Images\\Mars15.jpg,\\Images\\Mars16.jpg,\\Images\\Mars17.jpg,\\Images\\Mars18.jpg,",
                     Price = 1000000,
                     TourDuration = 2156
                 });;
+
+
+
+
+
+
+
 
             modelBuilder.Entity<ReviewModel>().HasData(
                new ReviewModel()
                {
                    Id = 1,
-                   Content = "It's okay",
+                   Content = "Awesome!! Love every minute. Definetly recomend it!!",
                    ReviewerName = "Kate Johnson",
                    ImageURL = "\\Images\\Kate.png",
-                   Timestamp = DateTime.ParseExact("2002-09-01", "yyyy-MM-dd", null),
+                   Timestamp = DateTime.ParseExact("2002-10-05", "yyyy-MM-dd", null),
                    StarRating = "5",
                    DestinationsId = 1
                },
-               new ReviewModel()
-               {
-                   Id = 2,
-                   Content = "It's decent",
-                   ReviewerName = "James Olson",
-                   ImageURL = "\\Images\\James.png",
-                   Timestamp = DateTime.ParseExact("2002-09-01", "yyyy-MM-dd", null),
-                   StarRating = "5",
-                   DestinationsId = 2
-               });
+                new ReviewModel()
+                {
+                    Id = 2,
+                    Content = "Best trip ever. I want to do it again!!! Friggin Awesome!!",
+                    ReviewerName = "John Chelios",
+                    ImageURL = "\\Images\\TokyoJohn.png",
+                    Timestamp = DateTime.ParseExact("2005-04-04", "yyyy-MM-dd", null),
+                    StarRating = "4",
+                    DestinationsId = 1
+                },
+                 new ReviewModel()
+                 {
+                     Id = 3,
+                     Content = "So glad i went. It was a trip of a lifetime. I want to do it again soon!!",
+                     ReviewerName = "Karen Smith",
+                     ImageURL = "\\Images\\TokyoKaren.png",
+                     Timestamp = DateTime.ParseExact("2010-09-09", "yyyy-MM-dd", null),
+                     StarRating = "5",
+                     DestinationsId = 1
+                 },
+                  new ReviewModel()
+                {
+                    Id = 4,
+                    Content = "Really enjoyed the sights and scenery. Glad I made the decision to go. Highly recomend it!",
+                      ReviewerName = "Hannah Simms",
+                    ImageURL = "\\Images\\TokyoHannah.png",
+                    Timestamp = DateTime.ParseExact("2007-09-01", "yyyy-MM-dd", null),
+                    StarRating = "4",
+                    DestinationsId = 1
+                },
+
+
+
+
+                   new ReviewModel()
+                   {
+                       Id = 5,
+                       Content = "Awesome!! Love every minute. Definetly recomend it!!",
+                       ReviewerName = "James Olson",
+                       ImageURL = "\\Images\\James.png",
+                       Timestamp = DateTime.ParseExact("2002-10-05", "yyyy-MM-dd", null),
+                       StarRating = "5",
+                       DestinationsId = 2
+                   },
+                new ReviewModel()
+                {
+                    Id = 6,
+                    Content = "Best trip ever. I want to do it again!!! Friggin Awesome!!",
+                    ReviewerName = "Eric Lindros",
+                    ImageURL = "\\Images\\RomeEric.png",
+                    Timestamp = DateTime.ParseExact("2005-04-04", "yyyy-MM-dd", null),
+                    StarRating = "4",
+                    DestinationsId = 2
+                },
+                 new ReviewModel()
+                 {
+                     Id = 7,
+                     Content = "So glad i went. It was a trip of a lifetime. I want to do it again soon!!",
+                     ReviewerName = "Fred Mayer",
+                     ImageURL = "\\Images\\RomeFred.png",
+                     Timestamp = DateTime.ParseExact("2010-09-09", "yyyy-MM-dd", null),
+                     StarRating = "5",
+                     DestinationsId = 2
+                 },
+                  new ReviewModel()
+                  {
+                      Id = 8,
+                      Content = "Really enjoyed the sights and scenery. Glad I made the decision to go. Highly recomend it!",
+                      ReviewerName = "Erika Stevenson",
+                      ImageURL = "\\Images\\RomeErika.png",
+                      Timestamp = DateTime.ParseExact("2007-09-01", "yyyy-MM-dd", null),
+                      StarRating = "4",
+                      DestinationsId = 2
+                  },
+
+
+
+
+                    new ReviewModel()
+                    {
+                        Id = 9,
+                        Content = "Awesome!! Love every minute. Definetly recomend it!!",
+                        ReviewerName = "Gary James",
+                        ImageURL = "\\Images\\ParisGary.jpg",
+                        Timestamp = DateTime.ParseExact("2002-10-05", "yyyy-MM-dd", null),
+                        StarRating = "5",
+                        DestinationsId = 3
+                    },
+                new ReviewModel()
+                {
+                    Id = 10,
+                    Content = "Best trip ever. I want to do it again!!! Friggin Awesome!!",
+                    ReviewerName = "Angela Salisbury",
+                    ImageURL = "\\Images\\ParisAngela.jpg",
+                    Timestamp = DateTime.ParseExact("2005-04-04", "yyyy-MM-dd", null),
+                    StarRating = "4",
+                    DestinationsId = 3
+                },
+                 new ReviewModel()
+                 {
+                     Id = 11,
+                     Content = "So glad i went. It was a trip of a lifetime. I want to do it again soon!!",
+                     ReviewerName = "Lily Lewis",
+                     ImageURL = "\\Images\\ParisLily.png",
+                     Timestamp = DateTime.ParseExact("2010-09-09", "yyyy-MM-dd", null),
+                     StarRating = "5",
+                     DestinationsId = 3
+                 },
+
+
+
+
+                   new ReviewModel()
+                   {
+                       Id = 12,
+                       Content = "Awesome!! Love every minute. Definetly recomend it!!",
+                       ReviewerName = "Kurt Jackson",
+                       ImageURL = "\\Images\\MarsKurt.png",
+                       Timestamp = DateTime.ParseExact("2002-10-05", "yyyy-MM-dd", null),
+                       StarRating = "5",
+                       DestinationsId = 4
+                   },
+                new ReviewModel()
+                {
+                    Id = 13,
+                    Content = "Best trip ever. I want to do it again!!! Friggin Awesome!!",
+                    ReviewerName = "Jim Norton",
+                    ImageURL = "\\Images\\MarsJim.png",
+                    Timestamp = DateTime.ParseExact("2005-04-04", "yyyy-MM-dd", null),
+                    StarRating = "4",
+                    DestinationsId = 4
+                },
+                 new ReviewModel()
+                 {
+                     Id = 14,
+                     Content = "So glad i went. It was a trip of a lifetime. I want to do it again soon!!",
+                     ReviewerName = "Steven Curtis",
+                     ImageURL = "\\Images\\MarsSpaceman.jpg",
+                     Timestamp = DateTime.ParseExact("2010-09-09", "yyyy-MM-dd", null),
+                     StarRating = "5",
+                     DestinationsId = 4
+                 },
+                new ReviewModel()
+                {
+                    Id = 15,
+                    Content = "It's decent",
+                    ReviewerName = "Lawrence Jackson",
+                    ImageURL = "\\Images\\MarsSpaceman 2.jpg",
+                    Timestamp = DateTime.ParseExact("2002-09-01", "yyyy-MM-dd", null),
+                    StarRating = "3",
+                    DestinationsId = 4
+                });
         }
     }
 }
